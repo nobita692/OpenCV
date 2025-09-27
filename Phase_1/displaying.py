b7718dc37ef644b940195fbce0c3ae755b471e55
Toufik Raja <toufikraja692@example.com>
2025-09-27 17:24:54 +0530

    Comment out

--- Phase_1/displaying.py
+++ Phase_1/displaying.py
@@ -2,9 +2,9 @@ import cv2
 image = cv2.imread("Phase_1/OpenCV Basics.png")
 
 if image is not None :
-    cv2.imshow("Image Showing",image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    cv2.imshow("Image Showing",image)#show the image
+    cv2.waitKey(0)#wait for a key
+    cv2.destroyAllWindows()#close the window
     
 else:
     print("Image not found")
--- Phase_1/loading.py
+++ Phase_1/loading.py
@@ -1,5 +1,5 @@
 import cv2
-image = cv2.imread("Phase_1/Tony.jpg")
+image = cv2.imread("Phase_1/Tony.jpg")#read the image
 
 if image is None:
     print("Error: Image path is empty")
